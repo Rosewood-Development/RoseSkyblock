b254c2e249133ce151605f2c1661a7fae4bd9225
Oribuin <>
2021-01-12 23:20:15 +0000

    Update NMSAdapter.kt

--- NMS/Wrapper/src/main/kotlin/dev/rosewood/roseskyblock/nms/NMSAdapter.kt
+++ NMS/Wrapper/src/main/kotlin/dev/rosewood/roseskyblock/nms/NMSAdapter.kt
@@ -4,15 +4,14 @@ import org.bukkit.Bukkit
 
 // If someone else wants to get this to be better, please do.
 object NMSAdapter {
-    var handler: NMSHandler? = null
-    var isValidVersion = false
+    lateinit var handler: NMSHandler
 
     init {
         try {
             val name = Bukkit.getServer().javaClass.getPackage().name
             val version = name.substring(name.lastIndexOf('.') + 1)
-            handler = Class.forName("dev.rosewood.roseskyblock.nms.$version.NMSHandlerImpl").getConstructor().newInstance() as NMSHandler
-        } catch (ignored: Exception) {
-        }
+            this.handler = Class.forName("dev.rosewood.roseskyblock.nms.$version.NMSHandlerImpl").getConstructor()
+                .newInstance() as NMSHandler
+        } catch (ignored: Exception) { }
     }
 }
