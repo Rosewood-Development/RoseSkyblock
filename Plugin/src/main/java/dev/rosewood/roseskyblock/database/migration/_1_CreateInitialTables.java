857e8ef4b8777c2b4695c618b60e2d692d483412
Robin <oribuinn@gmail.com>
2023-11-21 19:31:37 +0000

    Transform into batch statement

--- Plugin/src/main/java/dev/rosewood/roseskyblock/database/migration/_1_CreateInitialTables.java
+++ Plugin/src/main/java/dev/rosewood/roseskyblock/database/migration/_1_CreateInitialTables.java
@@ -6,6 +6,7 @@ import dev.rosewood.rosegarden.database.MySQLConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class _1_CreateInitialTables extends DataMigration {
 
@@ -17,38 +18,47 @@ public class _1_CreateInitialTables extends DataMigration {
     public void migrate(DatabaseConnector connector, Connection connection, String tablePrefix) throws SQLException {
         String autoIncrement = connector instanceof MySQLConnector ? " AUTO_INCREMENT" : "";
 
-        connection.createStatement().execute("CREATE TABLE " + tablePrefix + "island (" +
-                "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
-                "group_id INTEGER NOT NULL, " +
-                "world VARCHAR(100) NOT NULL, " +
-                "spawn_x DOUBLE NOT NULL, " +
-                "spawn_y DOUBLE NOT NULL, " +
-                "spawn_z DOUBLE NOT NULL, " +
-                "spawn_pitch DOUBLE NOT NULL, " +
-                "spawn_yaw FLOAT NOT NULL, " +
-                "UNIQUE (id, world), " +
-                "FOREIGN KEY (group_id) REFERENCES island_group(id))");
+        String islandData = "CREATE TABLE " + tablePrefix + "island (" +
+                            "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
+                            "group_id INTEGER NOT NULL, " +
+                            "world VARCHAR(100) NOT NULL, " +
+                            "spawn_x DOUBLE NOT NULL, " +
+                            "spawn_y DOUBLE NOT NULL, " +
+                            "spawn_z DOUBLE NOT NULL, " +
+                            "spawn_pitch DOUBLE NOT NULL, " +
+                            "spawn_yaw FLOAT NOT NULL, " +
+                            "UNIQUE (id, world), " +
+                            "FOREIGN KEY (group_id) REFERENCES island_group(id))";
 
-        connection.createStatement().execute("CREATE TABLE " + tablePrefix + "island_member (" +
-                "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
-                "group_id VARCHAR(36) NOT NULL, " +
-                "player_uuid VARCHAR(36) NOT NULL, " +
-                "member_level VARCHAR(20) NOT NULL, " +
-                "UNIQUE (ID, player_uuid), " +
-                "FOREIGN KEY (group_id) REFERENCES island_group(id)");
 
-        connection.createStatement().execute("CREATE TABLE " + tablePrefix + "island_group (" +
-                "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
-                "group_name VARCHAR(100) NOT NULL, " +
-                "owner_uuid VARCHAR(36) NOT NULL, " +
-                "location_id INTEGER NOT NULL, " +
-                "UNIQUE (group_name, owner_uuid), " +
-                "UNIQUE (group_name, location_id))");
+        String islandMember = "CREATE TABLE " + tablePrefix + "island_member (" +
+                              "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
+                              "group_id VARCHAR(36) NOT NULL, " +
+                              "player_uuid VARCHAR(36) NOT NULL, " +
+                              "member_level VARCHAR(20) NOT NULL, " +
+                              "UNIQUE (ID, player_uuid), " +
+                              "FOREIGN KEY (group_id) REFERENCES island_group(id)";
 
-        connection.createStatement().execute("CREATE TABLE " + tablePrefix + "island_settings (" +
-                "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
-                "border VARCHAR(10) NOT NULL)"
-        );
+        String islandGroup = "CREATE TABLE " + tablePrefix + "island_group (" +
+                             "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
+                             "group_name VARCHAR(100) NOT NULL, " +
+                             "owner_uuid VARCHAR(36) NOT NULL, " +
+                             "location_id INTEGER NOT NULL, " +
+                             "UNIQUE (group_name, owner_uuid), " +
+                             "UNIQUE (group_name, location_id))";
+
+        String islandSettings = "CREATE TABLE " + tablePrefix + "island_settings (" +
+                                "id INTEGER PRIMARY KEY " + autoIncrement + ", " +
+                                "border VARCHAR(10) NOT NULL)";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.addBatch(islandData);
+            statement.addBatch(islandMember);
+            statement.addBatch(islandGroup);
+            statement.addBatch(islandSettings);
+            statement.executeBatch();
+        }
+        
     }
 
 }
